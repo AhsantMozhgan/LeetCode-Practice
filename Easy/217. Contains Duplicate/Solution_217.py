bf3706e625551230f4dd776a042102e513e869ed
AhsantMozhgan <mozhganahsant@gmail.com>
2026-07-02 12:37:48 -0700

    217. Contains Duplicate

---
+++ Easy/217. Contains Duplicate/Solution_217.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        table = set()
+
+        for num in nums:
+
+            if num in table:
+                return True
+
+            table.add(num)
+            
+        return False
